ea201a440407f06716741c99afbb1faf7ba24f2a
Zubayed Ahmed <jubayedahmed2001@gmail.com>
2024-07-31 17:20:43 +0600

    Delete list_comprehension.py

--- list_comprehension.py
+++
@@ -1,7 +0,0 @@
-my_list=[1,2,3,4,5,6,7]
-update_list = [x**2 for x in my_list]
-print(update_list)
-
-my_list = [1,2,3,4,5,6,7]
-update_list = [x**2 for x in my_list if x%2==0 or x>3 if x**2>25]
-print(update_list)
